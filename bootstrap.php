0e725718aa53177fa7f52aa47feebfbd2671206f
domadla <domadlangbayan@gmail.com>
2025-07-08 18:00:46 +0800

    feat: added footer component and its path

--- bootstrap.php
+++ bootstrap.php
@@ -5,4 +5,6 @@ define('VENDOR_PATH', BASE_PATH . '/vendor/');
 define('HANDLERS_PATH', BASE_PATH . '/handlers/');
 define('DUMMIES_PATH', BASE_PATH . '/staticData/dummies/');
 define('LAYOUTS_PATH', BASE_PATH . '/layouts/');
+define('COMPONENTS_PATH', BASE_PATH . '/components/');
+
 chdir(BASE_PATH);
---
+++ components/componentGroup/footer.component.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+    &copy; <?= date('Y') ?> Domadla Meeting Calendar | All rights reserved.
+</footer>
