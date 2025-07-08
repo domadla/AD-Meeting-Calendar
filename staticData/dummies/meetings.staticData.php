a7f5eb7bd96339548b029b6f164825b563c7e476
domadla <domadlangbayan@gmail.com>
2025-07-08 17:12:27 +0800

    feat: Add initial static data for meetings

---
+++ staticData/dummies/meetings.staticData.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    [
+        'title'       => 'Project Meeting',
+        'description' => 'Team Meeting for the project',
+        'schedule'    => '2024-12-12 10:00:00',
+        'location'    => 'Main Office',
+        'created_by'  => 1,
+    ],
+];
